c7791ae3d7146e565c0154f238dd0a0546e3ff73
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-08-27 12:36:18 -0300

    modelos

---
+++ model/Resposta.js
@@ -0,0 +1,17 @@
+const { Sequelize, Model } = require("sequelize");
+const connection = require("../database/database");
+
+const Resposta = connection.define("respostas", {
+  corpo: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  perguntaId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+Resposta.sync({ force: false});
+
+module.exports = Resposta;
